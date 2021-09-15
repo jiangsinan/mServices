1a5de0db084939885c3d1531944fa1ac7fa9f09c
jiangsinan <2459140452@qq.com>
2021-09-16 00:07:06 +0800

    模板引入

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from tornado.web import Application
 
 from app.views.cookie_v import CookieHandler
@@ -5,11 +7,18 @@ from app.views.index_v import IndexHandler
 from app.views.order_v import OrderHandler
 from app.views.search_v import SearchHandler
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+settings = {
+    'debug':True,
+    'template_path':os.path.join(BASE_DIR,'templates')
+}
 
 def make_app(host='localhost'):
-    return Application([
+    return Application(handlers =[
         ('/', IndexHandler),
         ('/search', SearchHandler),
         ('/cookie', CookieHandler),
         (r'/order/(?P<action_code>\d+)/(?P<order_id>\d+)',OrderHandler),
-    ], default_host=host)
+    ],
+        default_host=host,**settings)
--- app/views/index_v.py
+++ app/views/index_v.py
@@ -2,18 +2,14 @@ from tornado.web import RequestHandler
 
 class IndexHandler(RequestHandler):
     def get(self):
+        self.render('index.html',msg='Hi，jsn')
+
+    def post(self):
         # wd = self.get_query_argument('wd')
         self.write('<h3>我是主页</h3>')
 
-    def post(self):
+    def gut(self):
         name = self.get_body_argument('name')
         city = self.get_body_argument('city')
         self.write('<h3>%s %s</h3>' % (name, city))
 
-    def delete(self):
-        print('delete')
-        self.write('<h3>我是delete</h3>')
-
-    def put(self):
-        print('put')
-        self.write('<h3>我是put</h3>')
